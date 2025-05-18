7bf7764c3761112b3a22c69428c7a841cf7a0c0f
reemibrahim17 <reemprograming@gmail.com>
2025-05-18 17:48:35 +0300

    added displayBoard method

--- tictactoc/bin/tictactoc.dart
+++ tictactoc/bin/tictactoc.dart
@@ -1,6 +1,9 @@
-import 'package:tictactoc/tictactoc.dart' as tictactoc;
 
-class TicTacToe {
+class TicTacToe
+ {
+ List<String>board=[" "," "," "," ", " ", " ", " "," "," "];
+ String curreentPlayer="x";
+ 
   startGame(){
     print("wlcome to Tic Tac Toe👋");
     print("this gama is played by 2 players");
@@ -8,4 +11,19 @@ class TicTacToe {
     print("player 2 is O");
     print("player 1 will start first"); 
   }
+displayBoard(){
+  for (int i = 0; i < 9; i+=3) {
+   print("${board[i]} | ${board[i+1]} | ${board[i+2]}");
+    if(i<6){
+      print("---------");
+    }
+  }
+
+}
+}
+void main(){
+TicTacToe game=TicTacToe();
+game.startGame();
+game.displayBoard();
+
 }
